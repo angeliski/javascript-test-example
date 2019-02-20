8ee25f223cbf7c36e2e00e61b506e5a21864f7df
Camila Ferreira <camila.ferreira@bluesoft.com.br>
2019-02-20 09:18:53 -0300

    Teste de converte.lista.js passando

--- converte.lista.js
+++ converte.lista.js
@@ -1,21 +1,27 @@
 function nome(pessoas) {
-	return pessoas.map((nome) => nome !== '');
+	return pessoas.map((nome) => nome.nome);
 }
 
 function nomeComNota(pessoas) {
-	
+	return pessoas.map((nome) => nome.nome + ' - ' + nome.nota);
 }
 
 function upperCase (pessoas) {
-  
+	return pessoas.map((nome) => nome.toUpperCase(nome));
 }
 
 function nomeComAprovacao (pessoas, notaParaAprovacao = 8) {
-  
-}
+  return pessoas.map((nome) => {
+    if(nome.nota > notaParaAprovacao){
+      return nome.nome + ' - ' + 'Aprovado'
+    }else{
+      return nome.nome + ' - ' + 'Reprovado'
+    }
+  })
+};
 
 function alunos(pessoas) {
-  
+  return pessoas.map((nome) => JSON.parse(JSON.stringify({ primeiroNome: nome.nome, notaFinal: nome.nota })));
 }
 
 module.exports = {

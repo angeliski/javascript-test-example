6774c771e8732caacad3b0847593ae73778cc413
Wesley Vitor Barbosa Barros <wesley.barros@bluesoft.com.br>
2019-02-20 09:22:28 -0300

    função ordenaPelaNotaAsc passou no teste

--- nota-util.js
+++ nota-util.js
@@ -6,22 +6,45 @@ const media = () => {
 
 
 function mediaTurma(alunos) {
-let notas = [];
-let somaTotalNotas = 0;
-let mediaTurma = 0;
-for(let i = 0; i< 4; i++){
-  
- notas[i] = alunos[i].notaFinal;
-}
-for(let i = 0; i<4; i++){
-  somaTotalNotas = somaTotalNotas + notas[i];
-}
-  return somaTotalNotas  / notas.length;
+  let notas = [];
+  let somaTotalNotas = 0;
+
+  for(let i = 0; i< 4; i++){
+
+    notas[i] = alunos[i].notaFinal;
+
+  }
+  for(let i = 0; i<4; i++){
+
+    somaTotalNotas = somaTotalNotas + notas[i];
+
+  }
+
+  return somaTotalNotas / notas.length;
 }
 
 
 function ordenaPelaNotaAsc(alunos) {
-    
+
+  let notas = [];
+  
+
+  for(let i = 0; i< 4; i++){
+
+    notas[i] = alunos[i].notaFinal;
+
+  }
+  let notasCrescentes = notas.sort(function(a, b){return a - b});
+
+  let objCrescente = [];
+  for (let i = 0; i < notas.length; i++){
+    for(let y = 0; y< 4; y++){
+      if(notasCrescentes[i] === alunos[y].notaFinal){
+        objCrescente[i] = alunos[y];
+      }
+    }
+  }
+  return objCrescente;
 }
 
 module.exports = {
